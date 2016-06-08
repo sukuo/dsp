1c65dc4813962300b8fa56af4be05fe1a7129e1e
Sunne Kuo <skuo989@gmail.com>
2016-06-08 16:54:31 -0400

    Update q6_strings.py

--- python/q6_strings.py
+++ python/q6_strings.py
@@ -124,6 +124,12 @@ def verbing(s):
 
 
 def not_bad(s):
+     index1 = word.find("not")
+     index2 = word.find("bad")
+     if "not" in word and "bad" in word and index2 > index1:
+         return word[:index1] +""+ "good" +""+ word[(index2+3):]
+     else:
+         return word
     """
     Given a string, find the first appearance of the substring 'not'
     and 'bad'. If the 'bad' follows the 'not', replace the whole
