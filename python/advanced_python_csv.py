b56466ce5e11e9655e329f0c4b4738824fa48d12
Sunne Kuo <skuo989@gmail.com>
2016-06-11 13:30:14 -0400

    Update advanced_python_csv.py

--- python/advanced_python_csv.py
+++ python/advanced_python_csv.py
@@ -1,1 +1,15 @@
-PLACE YOUR CODE HERE
+import csv
+import pandas as pd
+import numpy as np
+
+
+
+fac_cols = ['name', 'degree', 'title', 'email']
+fac_table = pd.read_csv('faculty.csv', 
+                        names = fac_cols, 
+                        header = None, 
+                        skiprows = [0] , 
+                        skipinitialspace=True)
+
+email = fac_table['email']
+email.to_csv('UPenn_emails.csv', index = False, header = False)
