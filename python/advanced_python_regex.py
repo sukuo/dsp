38da026af1267503c7c9d750c0419a9752e60fe7
Sunne Kuo <skuo989@gmail.com>
2016-06-10 23:33:52 -0400

    Update advanced_python_regex.py

--- python/advanced_python_regex.py
+++ python/advanced_python_regex.py
@@ -1,13 +1,10 @@
-PART1 
-
-Q1
 import csv
 import pandas as pd
 import numpy as np
 import re
+import pprint
 from collections import Counter
 
-degrees = []
 fac_cols = ['name', 'degree', 'title', 'email']
 fac_table = pd.read_csv('faculty.csv', 
                         names = fac_cols, 
@@ -17,12 +14,21 @@ fac_table = pd.read_csv('faculty.csv',
 
 degree = ' '.join(degree)
 degree = degree.split(" ")
-
-degree1 = [re.sub("Ph\.?D\.?", "PhD", elem) for elem in degree]
-degree2 = [re.sub("Sc\.?D\.?", "ScD", elem) for elem in degree1]
-
-freq = Counter(degree2)
+degree = [re.sub("Ph\.?D\.?", "PhD", elem) for elem in degree]
+degree = [re.sub("Sc\.?D\.?", "ScD", elem) for elem in degree]
+freq = Counter(degree)
 print(freq)
+print('\n')
 
 fac_table['title'] = fac_table['title'].str.replace('As{2}i.+', 'Assistant Professor of Biostatistics')
-fac_table.groupby('title').count()
+print(fac_table.groupby('title').count())
+print('\n')
+
+emails = fac_table['email'].tolist()
+pprint.pprint(emails)
+print('\n')
+emails = ' '.join(emails)
+emails = re.split(("@|\s"), emails )
+email_types = [i for i in emails if '.edu' in i]
+email_freq = Counter(email_types)
+email_freq
